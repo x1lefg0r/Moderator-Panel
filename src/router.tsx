52d45a95a08007398dcc43b909ef4e420bee586e
x1lefg0r <ebalynin@mail.ru>
2026-02-15 02:46:48 +0300

    fix: added basename in router

--- src/router.tsx
+++ src/router.tsx
@@ -5,37 +5,42 @@ const ListPage = lazy(() => import("./pages/ListPage"));
 const ItemPage = lazy(() => import("./pages/ItemPage"));
 const StatsPage = lazy(() => import("./pages/StatsPage"));
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Navigate to="/list" replace />,
+    },
+    {
+      path: "list",
+      element: (
+        <Suspense fallback={<div>Loading...</div>}>
+          <ListPage />
+        </Suspense>
+      ),
+    },
+    {
+      path: "item/:id",
+      element: (
+        <Suspense fallback={<div>Loading...</div>}>
+          <ItemPage />
+        </Suspense>
+      ),
+    },
+    {
+      path: "stats",
+      element: (
+        <Suspense fallback={<div>Loading...</div>}>
+          <StatsPage />
+        </Suspense>
+      ),
+    },
+    {
+      path: "*",
+      element: <div>404 Not Found</div>,
+    },
+  ],
   {
-    path: "/",
-    element: <Navigate to="/list" replace />,
+    basename: import.meta.env.BASE_URL,
   },
-  {
-    path: "list",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <ListPage />
-      </Suspense>
-    ),
-  },
-  {
-    path: "item/:id",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <ItemPage />
-      </Suspense>
-    ),
-  },
-  {
-    path: "stats",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <StatsPage />
-      </Suspense>
-    ),
-  },
-  {
-    path: "*",
-    element: <div>404 Not Found</div>,
-  },
-]);
+);
