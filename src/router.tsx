33786aafa2185d9f55e5453c33be35daab119fe4
x1lefg0r <ebalynin@mail.ru>
2026-02-15 17:46:52 +0300

    fix: duplicate url string

--- src/router.tsx
+++ src/router.tsx
@@ -41,6 +41,6 @@ export const router = createBrowserRouter(
     },
   ],
   {
-    basename: import.meta.env.BASE_URL === "/" ? "/" : "/Moderator-Panel",
+    basename: import.meta.env.BASE_URL,
   },
 );
